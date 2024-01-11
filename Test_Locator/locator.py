7b21f5f3eabefc9659dbfcd04fa4cf1bb63c258f
Sheema-parveen <sheemaparveenfeb14@gmail.com>
2024-01-11 14:04:02 +0530

    All elements locator are stored here

---
+++ Test_Locator/locator.py
@@ -0,0 +1,20 @@
+class Locator():
+     url="https://www.saucedemo.com/"
+     username = "user-name"
+     password ="password"
+     login = "login-button"
+     prod_1 ="add-to-cart-sauce-labs-backpack"
+     prod_2="add-to-cart-sauce-labs-bike-light"
+     two='//*[@id="shopping_cart_container"]/a/span'
+     remove="remove-sauce-labs-bike-light"
+     exist='//*[@id="item_4_title_link"]/div'
+     check="checkout"
+     firstname="first-name"
+     lastname="last-name"
+     zip='postal-code'
+     continue_button="continue"
+     finish="finish"
+     cancel="cancel"
+     back="back-to-products"
+
+
